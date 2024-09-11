05bec1a54cadd21f6d68e0c56eb745b2346b1c6f
Tomer <tomer.cohen243@gmail.com>
2024-09-11 13:21:21 +0300

    intialized project

---
+++ simulator_main/Controllers/BitstreamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using simulator_main.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simulator_main.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BitstreamController : ControllerBase
+    {
+        IBitstreamService _BitstreamService;
+        public BitstreamController(IBitstreamService bitstreamService)
+        {
+            _BitstreamService = bitstreamService;
+        }
+
+
+        [HttpGet("getSimulation{icdId}")]
+       
+        public async Task<string> GetBitstream()
+        {
+            return await _BitstreamService.GetBitstream();
+        }
+    }
+}
